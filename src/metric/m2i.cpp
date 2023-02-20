d43dc570b506384a76f1a6fced3e6063d1c638df
lionlai <lionlai1989@gmail.com>
2023-02-20 05:44:38 +0100

    update

--- src/metric/m2i.cpp
+++ src/metric/m2i.cpp
@@ -1,5 +1,7 @@
 #include <src/metric/m2i.h>
 
-double mm2inch(double mm) {
-  return mm / 25.3;
-}
+namespace src::metric {
+
+double mm2inch(double mm) { return mm / 25.3; }
+
+} // namespace src::metric
--- src/metric/m2i.h
+++ src/metric/m2i.h
@@ -1,6 +1,8 @@
 #ifndef METRIC_M2I_H
 #define METRIC_M2I_H
 
+namespace src::metric {
 double mm2inch(double mm);
+}
 
 #endif

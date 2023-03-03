5cef26d3b70bfef4da82bf3c499e495b77d2de78
lionlai <lionlai1989@gmail.com>
2023-03-03 09:40:21 +0100

    update

--- tests/src/geometry/shapes/square_test.cpp
+++ tests/src/geometry/shapes/square_test.cpp
@@ -1,10 +1,8 @@
-#include <geometry/shapes/square.hpp>
 #include <gtest/gtest.h>
 #include <iostream>
+#include <src/geometry/shapes/square.hpp>
 
 TEST(SquareTest, ClassAssertion) {
   src::geometry::shapes::Square s{2.0};
-  s.display();
-
-  EXPECT_EQ(s.get_area(), 4.0);
+  EXPECT_EQ(s.get_area(), 2 * 2 * 25.3 * 25.3);
 }

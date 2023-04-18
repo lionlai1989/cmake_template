fa59a5d5ebb978b52e7ff3a721efc3e662f4839a
lionlai <lionlai1989@gmail.com>
2023-04-18 08:36:13 +0200

    Add unittest for move constructor.

--- tests/mypackage/image/test_image_xtensor.cpp
+++ tests/mypackage/image/test_image_xtensor.cpp
@@ -4,34 +4,57 @@
 #include <vector>
 #include <xtensor/xadapt.hpp>
 
+mypackage::image::ImageXTensor get_ones_image(int channel, int height,
+                                              int width) {
+  auto img = mypackage::image::ImageXTensor(channel, height, width);
+  *(img.pixels) += 1;
+  return img;
+}
+
 TEST(ImageXTensor, ClassAssertion) {
-  mypackage::image::ImageXTensor test_img1(2, 3, 4);
+  mypackage::image::ImageXTensor test_img(2, 3, 4);
   int counter = 0;
-  for (int x = 0; x < test_img1.width; x++) {
-    for (int y = 0; y < test_img1.height; y++) {
-      for (int c = 0; c < test_img1.channels; c++) {
-        (*test_img1.pixels)(c, y, x) = counter;
+  for (int x = 0; x < test_img.width; x++) {
+    for (int y = 0; y < test_img.height; y++) {
+      for (int c = 0; c < test_img.channels; c++) {
+        (*test_img.pixels)(c, y, x) = counter;
         ++counter;
       }
     }
   }
-  std::clog << "test_img1:\n" << (*test_img1.pixels) << '\n';
+  std::clog << "test_img:\n" << (*test_img.pixels) << '\n';
 
   std::clog << "Test Copy Constructor.\n";
-  mypackage::image::ImageXTensor test_img2{test_img1};
-  std::clog << "test_img2:\n" << (*test_img2.pixels) << '\n';
-  EXPECT_EQ(test_img2, test_img1);
+  mypackage::image::ImageXTensor img_cp_ctor{test_img};
+  std::clog << "img_cp_ctor:\n" << (*img_cp_ctor.pixels) << '\n';
+  EXPECT_EQ(img_cp_ctor, test_img);
 
   std::clog << "Test Copy Assignment Operator.\n";
-  mypackage::image::ImageXTensor test_img3;
-  test_img3 = test_img1;
-  std::clog << "test_img3\n" << (*test_img3.pixels) << '\n';
-  EXPECT_EQ(test_img3, test_img1);
+  mypackage::image::ImageXTensor img_cp_asgn_otor;
+  img_cp_asgn_otor = test_img;
+  std::clog << "img_cp_asgn_otor\n" << (*img_cp_asgn_otor.pixels) << '\n';
+  EXPECT_EQ(img_cp_asgn_otor, test_img);
 
   std::clog << "Test Move Assignment Operator.\n";
-  mypackage::image::ImageXTensor test_img4;
-  test_img4 = std::move(test_img1);
-  // test_img1 becomes unspecified after it's moved from.
-  EXPECT_EQ(test_img1.pixels, nullptr);
-  EXPECT_EQ(test_img4, test_img2);
+  mypackage::image::ImageXTensor img_mv_asgn_otor;
+  img_mv_asgn_otor = std::move(test_img);
+  // test_img becomes unspecified after it's moved from.
+  EXPECT_EQ(test_img.pixels, nullptr);
+  EXPECT_EQ(img_mv_asgn_otor, img_cp_ctor);
+
+  std::clog << "Test Move Constructor.\n";
+  /**
+   * NOTE: Move Constructor is not called due to Return Value Optimization.
+   * https://en.cppreference.com/w/cpp/language/copy_elision
+   */
+  mypackage::image::ImageXTensor img_mv_ctor{get_ones_image(2, 3, 4)};
+  std::clog << "img_mv_ctor:\n" << (*img_mv_ctor.pixels) << '\n';
+  /**
+   * NOTE: Move Constructor is called because there is no instance construction.
+   */
+  std::vector<mypackage::image::ImageXTensor> vec;
+  vec.push_back(get_ones_image(2, 3, 4));
+  std::clog << "vec[0]:\n" << *(vec[0].pixels) << '\n';
+  mypackage::image::ImageXTensor tmp_vec = get_ones_image(2, 3, 4);
+  EXPECT_EQ(tmp_vec, vec[0]);
 }
